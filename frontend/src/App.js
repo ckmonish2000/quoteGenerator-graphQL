44103dc4ba12c403b2167baa53fbdea57c126c36
ckmonish2000 <ckmonish2000@gmail.com>
2020-08-10 00:55:20 +0530

    working quote generator

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
-import Spacex from "./Spacex";
+// import Spacex from "./Spacex";
+import Quote from "./quote";
 
 export default class App extends Component {
   render() {
-    return <Spacex />;
+    return <Quote />;
   }
 }
--- frontend/src/quote.js
+++ frontend/src/quote.js
@@ -1,10 +1,42 @@
 import React, { Component } from "react";
-
+import { InMemoryCache, ApolloClient, gql } from "@apollo/client";
 export default class Quote extends Component {
+  state = {
+    quote: "load quotes",
+  };
+
+  client = new ApolloClient({
+    uri: "https://mkgraphql1.herokuapp.com/graphql",
+    cache: new InMemoryCache(),
+  });
+
+  call = () => {
+    this.client
+      .query({
+        query: gql`
+          query newquote {
+            quote
+          }
+        `,
+      })
+      .then((result) => {
+        console.log(result.data.quote);
+        this.setState({ quote: result.data.quote });
+      });
+  };
+
   render() {
     return (
       <div>
-        <h1>{this.props.quote}</h1>
+        <h1>{this.state.quote}</h1>
+        <button onClick={this.call}>get quote</button>
+        <button
+          onClick={() => {
+            this.setState({ quote: "load quotes" });
+          }}
+        >
+          reset
+        </button>
       </div>
     );
   }
