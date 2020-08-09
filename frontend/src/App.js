c3a30cf31196381ca77cfa4d87119d22619ea6fd
ckmonish2000 <ckmonish2000@gmail.com>
2020-08-10 00:05:30 +0530

    component spacex

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,44 +1,8 @@
 import React, { Component } from "react";
-import { ApolloClient, InMemoryCache } from "@apollo/client";
-import { gql } from "@apollo/client";
+import Spacex from "./Spacex";
 
 export default class App extends Component {
-  client = new ApolloClient({
-    uri: "https://api.spacex.land/graphql/",
-    cache: new InMemoryCache(),
-  });
-
-  state = {
-    dta: "hello",
-  };
-
-  call = () => {
-    this.client
-      .query({
-        query: gql`
-          query upcoming {
-            launchesUpcoming {
-              id
-              rocket {
-                rocket_name
-                rocket_type
-              }
-            }
-          }
-        `,
-      })
-      .then((result) => {
-        console.log(result);
-        this.setState({ dta: "working" });
-      });
-  };
   render() {
-    console.log("bro");
-    return (
-      <div>
-        <h1>{this.state.dta}</h1>
-        <button onClick={this.call}>click</button>
-      </div>
-    );
+    return <Spacex />;
   }
 }
---
+++ frontend/src/Spacex.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { gql } from "@apollo/client";
+
+export default class Spacex extends Component {
+  client = new ApolloClient({
+    uri: "https://api.spacex.land/graphql/",
+    cache: new InMemoryCache(),
+  });
+
+  state = {
+    dta: "hello",
+  };
+
+  call = () => {
+    this.client
+      .query({
+        query: gql`
+          query upcoming {
+            launchesUpcoming {
+              id
+              rocket {
+                rocket_name
+                rocket_type
+              }
+            }
+          }
+        `,
+      })
+      .then((result) => {
+        console.log(result);
+        this.setState({ dta: "working" });
+      });
+  };
+  render() {
+    console.log("bro");
+    return (
+      <div>
+        <h1>{this.state.dta}</h1>
+        <button onClick={this.call}>click</button>
+      </div>
+    );
+  }
+}
